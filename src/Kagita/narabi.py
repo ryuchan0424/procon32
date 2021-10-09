e7a67066412e08f6d3db73828541b83f5cecfdbd
KagitaHaruyo <92096951+KagitaHaruyo@users.noreply.github.com>
2021-10-09 21:46:19 +0900

    3X3を解けるようになりました

--- src/Kagita/narabi.py
+++ src/Kagita/narabi.py
@@ -3,12 +3,20 @@ import cv2
 import random
 import sys
 
-index=np.array([[2,3,0],[4,7,6],[3,8,1]])
+
+r=[[2,0],[2,1],[2,2],[1,2],[0,2],[0,1],[0,0],[1,0]]
+index=np.array([[0,3,2],[4,7,6],[1,8,5]])
 mokuhyo=np.array([[3,2,1],[4,0,8],[5,6,7]])
+cansame=[False,False,False,False,False,False,False,True,True]
 
+for i in range(3):
+ memo=""
+ for j in range(3):
+  memo=memo+str(index[j,i])+","
+ print(memo)
 
-r=[[2,1],[2,2],[1,2],[0,2],[0,1],[0,0],[1,0],[2,0]]
-textm=["R\n","D\n","L\n","U\n"]
+print()
+textm=["R","D","L","U"]
 idm=[[1,0],[0,1],[-1,0],[0,-1]]
 text=""
 #0 1
@@ -17,123 +25,123 @@ sta1=[0,1,2,3]
 #3 4 5 6 7 8 +3?
 sta2=[2,1,1,0,0,3,2]
 
-
 ox=1
 oy=1
 
-if index[1,1]!=0:
+def narabi(ox,oy,n,index,text):
+ memo=index[ox,oy]
+ index[ox,oy]=index[ox+idm[n][0],oy+idm[n][1]]
+ index[ox+idm[n][0],oy+idm[n][1]]=memo
+ 
+ ox=ox+idm[n][0]
+ oy=oy+idm[n][1]
+ 
+ text=text+textm[n]
+ return ox,oy,index,text
+
+if index[1,1]!=0:#0を真ん中に持っていく
  m=-1
  for i in range(4):
-  if index[r[i*2+1][0],r[i*2+1][1]]==0:
-   ox=r[i*2+1][0]
-   oy=r[i*2+1][1]
-   
-   i=i+2
-   if i>=4:
-    i=i-4
-   
-   memo=index[ox,oy]
-   index[ox,oy]=index[ox+idm[i][0],oy+idm[i][1]]
-   index[ox+idm[i][0],oy+idm[i][1]]=memo
-   
-   ox=ox+idm[i][0]
-   oy=oy+idm[i][1]
-   
-   text=text+textm[i]
-   break
- for i in range(4):
   if index[r[i*2][0],r[i*2][1]]==0:
+   ox=r[i*2][0]
+   oy=r[i*2][1]
+   i=i+1
+   if i>=4:
+    i=i-4
+   ox,oy,index,text=narabi(ox,oy,i,index,text)
+   break
+
+ for i in range(4):
+  if index[r[i*2+1][0],r[i*2+1][1]]==0:
    i=i+2
    if i>=4:
     i=i-4
-   
-   memo=index[ox,oy]
-   index[ox,oy]=index[ox+idm[i][0],oy+idm[i][1]]
-   index[ox+idm[i][0],oy+idm[i][1]]=memo
-   
-   ox=ox+idm[i][0]
-   oy=oy+idm[i][1]
-   
-   text=text+textm[i]
-    
+   ox,oy,index,text=narabi(ox,oy,i,index,text)
+   break
 
 
-for g in range(4):
- if index[r[g*2+1][0],r[g*2+1][1]]==1:
-  g=g+3
+for i in range(4):#1を真ん中列に持っていく
+ if index[r[i*2][0],r[i*2][1]]==1:
   for j in range(4):
-   g=g+1
-   if g>=4:
-    g=g-4
-   
-   memo=index[ox,oy]
-   index[ox,oy]=index[ox+idm[g][0],oy+idm[g][1]]
-   index[ox+idm[g][0],oy+idm[g][1]]=memo
-   
-   ox=ox+idm[g][0]
-   oy=oy+idm[g][1]
-   
-   text=text+textm[g]
-    
+   n=j+3
+   if n>=4:
+    n=n-4
+   n=n+i
+   if n>=4:
+    n=n-4
+   ox,oy,index,text=narabi(ox,oy,n,index,text)
+  break
 
-'''
+for i in range(4):#1を真ん中に持っていく
+ if index[r[i*2+1][0],r[i*2+1][1]]==1:
+  n=i
+  ox,oy,index,text=narabi(ox,oy,n,index,text)
+  break
 
-for g in range(4):
- if index[r[g*2][0],r[g*2][1]]==1:
-  memo=index[ox,oy]
-  index[ox,oy]=index[ox+idm[g][0],oy+idm[g][1]]
-  index[ox+idm[g][0],oy+idm[g][1]]=memo
-  
-  ox=ox+idm[g][0]
-  oy=oy+idm[g][1]
-   
-  text=text+textm[g]
-  
-  print()
-  print(g)
+for i in range(4):
+ if index[r[i*2+1][0],r[i*2+1][1]]==0:
+  for j in range(7-2*(3-i)):
+   n=sta2[2*(3-i)+j]
+   ox,oy,index,text=narabi(ox,oy,n,index,text)
+  break
+ 
+for j in range(4):
+ n=j+3
+ if n>=4:
+  n=n-4
+ ox,oy,index,text=narabi(ox,oy,n,index,text)
 
-  for j in range(7-(3-g)*2):
-   n=sta2[j+(3-g)*2]
-   print(n)
-   memo=index[ox,oy]
-   index[ox,oy]=index[ox+idm[n][0],oy+idm[n][1]]
-   index[ox+idm[n][0],oy+idm[n][1]]=memo
-   
-   ox=ox+idm[n][0]
-   oy=oy+idm[n][1]
-   
-   text=text+textm[n]
+order=[8,7,2,3,4,5,6,7,8]
+buf=[0,1,2,3,4,5,6,7,8]
 
-
-for i in range(1):
- for g in range(4):
-  if index[r[g*2+1][0],r[g*2+1][1]]==i+1:
-   g=g+3
+for h in range(len(order)):
+ g=buf[order[h]]##入れ替え記録
+ if h>=2:
+  for i in range(4):
+   if cansame[g] and cansame[index[r[i*2+1][0],r[i*2+1][1]]] :
+    memo=buf[g]
+    buf[g]=index[r[i*2+1][0],r[i*2+1][1]]
+    buf[index[r[i*2+1][0],r[i*2+1][1]]]=memo
+    g=index[r[i*2+1][0],r[i*2+1][1]]
+ for i in range(4):#1を真ん中列に持っていく
+  if index[r[i*2][0],r[i*2][1]]==g:
    for j in range(4):
-    g=g+1
-    if g>=4:
-     g=g-4
+    n=j+3
+    if n>=4:
+     n=n-4
+    n=n+i
+    if n>=4:
+     n=n-4
     
-    memo=index[ox,oy]
-    index[ox,oy]=index[ox+idm[g][0],oy+idm[g][1]]
-    index[ox+idm[g][0],oy+idm[g][1]]=memo
-    
-    ox=ox+idm[g][0]
-    oy=oy+idm[g][1]
-    
-    text=text+textm[g]
-'''
+    ox,oy,index,text=narabi(ox,oy,n,index,text)
+   break
+ 
+ for i in range(4):#1を真ん中に持っていく
+  if index[r[i*2+1][0],r[i*2+1][1]]==g:
+   n=i
+
+   ox,oy,index,text=narabi(ox,oy,n,index,text)
+   break
+
+ for i in range(4):
+  if index[r[i*2+1][0],r[i*2+1][1]]==0:
+   for j in range(7-2*(3-i)):
+    n=sta2[2*(3-i)+j]
+    ox,oy,index,text=narabi(ox,oy,n,index,text)
+   break
+ if h>=2:
+  print(g)
+  print(buf)
+  cansame[g]=False
 
 for i in range(3):
  memo=""
  for j in range(3):
-  memo=memo+str(index[i,j])+","
+  memo=memo+str(index[j,i])+","
  print(memo)
- 
+print() 
+
 print(ox)
 print(oy)
 
 print(text)
-   
-   
-    
